aebf18a5d50fb9eced394e9e131f05e402089e99
Tiago Davi <tiago.asp.net@gmail.com>
2022-07-22 01:00:19 -0300

    Initial Commit

---
+++ src/pages/api/cards.js
@@ -0,0 +1,34 @@
+
+export default (req, res) => {
+  if (req.method === "POST"){
+    post(req, res);
+  }else{
+    res.status(405).send();
+  }
+}
+
+
+function post(req, res){
+  const body = JSON.parse(req.body);
+  
+  fetchAnimals(body.number)
+  .then((animals) => {
+    res.status(200).json({
+      name: body.name,
+      animals: animals
+    });
+  });
+}
+
+function fetchAnimals(number) {
+  return fetch(`https://zoo-animal-api.herokuapp.com/animals/rand/${number}`)
+  .then(resp => resp.json())
+  .then(res => res.map(animal => {
+    return {
+      id: animal.id,
+      name: animal.name,
+      description: animal.animal_type,
+      image: animal.image_link
+    };
+  }));
+}
